718b543bf40224392660f15aa838d6abc189d510
byshiny <bs5ht@virginia.edu>
2018-03-15 12:34:04 -0400

    mouse listener works

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 
 
 var electron = require('electron');
+/*
 var app = electron.app;
-const {BrowserWindow, ipcMain} = require('electron')
+const {BrowserWindow, ipcMain} = require('electron');
+var configuration = require('./configuration');
 var mainWindow = null;
 
 app.on('ready', function() {
@@ -15,8 +17,44 @@ app.on('ready', function() {
     });
 
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+    if (!configuration.readSettings('shortcutKeys')) {
+        configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
+    }
 });
 
 ipcMain.on('close-main-window', function () {
     app.quit();
 });
+*/
+'use strict';
+const ioHook = require('iohook');
+
+ioHook.on("mousemove", event => {
+  console.log(event);
+  /* You get object like this
+    {
+      type: 'mousemove',
+      x: 700,
+      y: 400
+    }
+   */
+});
+
+//Register and start hook
+ioHook.start();
+
+/*
+function setGlobalShortcuts() {
+    globalShortcut.unregisterAll();
+
+    var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
+    var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
+
+    globalShortcut.register(shortcutPrefix + '1', function () {
+        mainWindow.webContents.send('global-shortcut', 0);
+    });
+    globalShortcut.register(shortcutPrefix + '2', function () {
+        mainWindow.webContents.send('global-shortcut', 1);
+    });
+    */
