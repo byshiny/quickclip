4575ebd79e7d7990ffba6e43478628ca44e37331
byshiny <bs5ht@virginia.edu>
2018-06-30 15:43:21 -0400

    significant proress made. Made fade out to work, working on mechanism to copy and paste to buffers

--- main.js
+++ main.js
@@ -10,13 +10,22 @@ STEPS:
 */
 
 /* GLOBAL Parameters
-*/
+ */
 console.log(process.version)
 var COPY_BUFFER_SIZE = 10;
 
 const ioHook = require('iohook');
-const {app, BrowserWindow, ipcMain, globalShortcut} = require('electron')
-const {clipboard, dialog} = require('electron')
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  globalShortcut
+} = require('electron')
+const {
+  clipboard,
+  dialog
+} = require('electron')
+
 const configuration = require('./configuration')
 
 var robot = require("robotjs")
@@ -26,6 +35,9 @@ var CircularBuffer = require("circular-buffer");
 
 var copyBuf = new CircularBuffer(COPY_BUFFER_SIZE);
 
+var textBufferArray = new Array(10);
+var textBufferChecker = new Array(10);
+var textBufferTimer = new Array(10);
 
 var readString = clipboard.readText();
 console.log("copy text");
@@ -35,63 +47,63 @@ copyBuf.enq(readString);
 
 
 
-  // Or use `remote` from the renderer process.
-  // const {BrowserWindow} = require('electron').remote
+// Or use `remote` from the renderer process.
+// const {BrowserWindow} = require('electron').remote
 
 
 var mainWindow = null;
 let bufferWindow = null;
-  // Load a remote URL
-  app.on('ready', () => {
+// Load a remote URL
+app.on('ready', () => {
 
-    //need to externalize window size
-
-    mainWindow = new BrowserWindow({width: 800, height: 600})
-    mainWindow.on('closed', () => {
-      mainWindow = null
-    })
-    mainWindow.loadURL('https://github.com')
-    setGlobalShortcuts();
-    //REMOVE THIS LATER: mainWindow.hide()
-    //let win = new BrowserWindow({transparent: true, frame: false})
-    //win.show()
-
-
-    ioHook.start();
+  //need to externalize window size
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+  mainWindow.loadURL('https://github.com')
+  setGlobalShortcuts();
+  //REMOVE THIS LATER: mainWindow.hide()
+  //let win = new BrowserWindow({transparent: true, frame: false})
+  //win.show()
+  ioHook.start();
 })
 
 var content = clipboard.readText();
 console.log("keyboard content" + content);
 
-ipcMain.on('close-main-window', function () {
-    app.quit();
+ipcMain.on('close-main-window', function() {
+  app.quit();
 });
-
-
-
-
-
 
 
 var copyBuffer = new CircularBuffer(COPY_BUFFER_SIZE);
 
 var currentEvent = null;
 var mouseDown = false;
+/*
 ioHook.on('keydown', event => {
   console.log(event); // { type: 'mousemove', x: 700, y: 400 }
   showBuffer();
 });
-
-/* This function shows the current window that is available
 */
+/* This function shows the current window that is available
+ */
 
-function showBuffer(){
+function showBuffer() {
   //TODO: Parameterize width and height
-  bufferWindow = new BrowserWindow({width: 400, height: 200});
+  bufferWindow = new BrowserWindow({
+    width: 400,
+    height: 200,
+    transparent: false
+  })
   bufferWindow.loadURL(`file://${__dirname}/resources/app/index.html`);
   bufferWindow.webContents.on('did-finish-load', function() {
     bufferWindow.webContents.send('load-buffer', readString);
-});
+  });
 }
 
 
@@ -105,48 +117,75 @@ ioHook.on("mousewheel", event => {
 
 });
 
-
-/*
 ioHook.on("mouseup", event => {
   console.log(event);
   currentEvent = event;
   mouseDown = false;
   mainWindow = null
 });
-*/
+
+
+ioHook.on("keyup", e => {
+  console.log(e.rawcode, String.fromCharCode(e.rawcode));
+});
+
 //Register and start hook
 
 
 
-function callEvent()
-{
- if(mouseDown)
- {
-   console.log("hellooo");
-   // do whatever you want
-   // it will continue executing until mouse is not released
+function callEvent() {
+  if (mouseDown) {
+    console.log("hellooo");
+    // do whatever you want
+    // it will continue executing until mouse is not released
 
 
-   setTimeout(callEvent,1000);
- }
- else
- return;
+    setTimeout(callEvent, 1000);
+  } else {
+    return;
+  }
 }
 
 
-function startListeners(){
+function startListeners() {
 
 
 
 }
 
 function setGlobalShortcuts() {
-    globalShortcut.unregisterAll();
+  globalShortcut.unregisterAll();
 
-    var shortcutKeysSettingArray = configuration.readSettings('pasteBuffer1');
-    var shortcutKeysSetting = shortcutKeysSettingArray[0];
-    globalShortcut.register(shortcutKeysSetting, function () {
-        mainWindow.webContents.send('global-shortcut', 0);
-    });
+  var shortcutKeysSettingArray = configuration.readSettings('pasteBuffer1');
+  var shortcutKeysSetting = shortcutKeysSettingArray[0];
+  console.log('paste buffer 1' + shortcutKeysSetting);
+  globalShortcut.register(shortcutKeysSetting, function() {
+    mainWindow.webContents.send('global-shortcut', 0);
+  });
+  //pop up paste buffer
+  globalShortcut.register('n', function() {
+    showBuffer();
+  });
+  globalShortcut.register('m', function() {
+    bufferWindow.webContents.send('inc-opq', readString);;
+  });
+  globalShortcut.register('Ctrl+1', function() {
+    readString = clipboard.readText();
+    textBufferChecker[0] = 1;
+    textBufferArray[0] = readString;
+    textBufferTimer[0] = new Date();
+    console.log("Content saved");
+    console.log(readString);
+  });
+  globalShortcut.register('CtrlOrCommand+c+1', function() {
+    readString = clipboard.readText();
+    textBufferArray[0] = readString;
+    console.log("Content saved");
+    console.log(readString);
+  });
+  globalShortcut.register('Alt+1', function() {
+    console.log("Robotjs pasted");
+    robot.keyTap('v', ['command']);
+  });
 
-  }
+}
